ca913ab552da5a055ad538230306a2a4dea718f8
Benjamin Gaignard <benjamin.gaignard@linaro.org>
2012-10-09 13:39:25 +0200

    gpu: ion: fix compilation warning
    use atomic_read to get the refcount value to avoid compilation warning
    
    Change-Id: I9bf0a07c787af8f87ac19314f996a78142f72f6b
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -1421,8 +1421,7 @@ static int ion_debug_heap_show(struct seq_file *s, void *unused)
 			continue;
 		total_size += buffer->size;
 		if (!buffer->handle_count) {
-			seq_printf(s, "%16.s %16u %16u %d %d\n",
-				   buffer->task_comm,
+			seq_printf(s, "%16.s %16u %16u %d %d\n", buffer->task_comm,
 				   buffer->pid, buffer->size, buffer->kmap_cnt,
 				   atomic_read(&buffer->ref.refcount));
 			total_orphaned_size += buffer->size;
