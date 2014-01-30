f1d47bb3bf5b31f31b9522f50055e7fc421357d7
OP_Nay <opnayn@gmail.com>
2014-01-30 12:58:11 +0900

    build: Revert Make Build Function

--- build.sh
+++ build.sh
@@ -24,43 +24,6 @@ function echo_notify() {
 	echo -e "\e[33m$*\e[00m"
 }
 
-#Build function
-function build_clean() {
-	#Cean directory
-	rm -rf $KERNDIR/out_bootimg $KERNDIR_OUT
-	mkdir $KERNDIR/out_bootimg $KERNDIR_OUT $INITRAM_DIR
-
-	#Clean objects
-	#is not used
-	#make distclean
-}
-
-function build_defconfig() {
-	make $1 menuconfig O=$KERNDIR_OUT
-}
-
-function build_initramfs() {
-	# copy original initramfs directory to kernel directory
-	cp -r $1/* $INITRAM_DIR/
-	# remove backup files
-	find $INITRAM_DIR -name "*~" -exec rm -rf {} \;
-
-	# find and copy modulized files
-	for module_file in `find $KERNDIR_OUT -name "*.ko"`
-	do
-		echo $module_file
-		"$CROSS_COMPILE"strip --strip-unneeded $module_file
-		cp $module_file $INITRAM_DIR/lib/modules/
-	done
-	# make initramfs file
-	cd $INITRAM_DIR
-	find . | cpio -o -H newc | gzip > $KERNDIR/out_bootimg/ramdisk.cpio.gz
-}
-
-function build_bootimg() {
-	$KERNDIR/mkbootimg --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --kernel $KERNDIR_OUT/arch/arm/boot/zImage --ramdisk ramdisk.cpio.gz -o boot.img
-}
-
 if [ "$1" == "" ]
 then
 	echo_error "build.sh [ skt / kt / lg ]"
@@ -95,9 +58,10 @@ fi
 
 
 echo_notify "----------------------------------------------------------------------------------------------------------CLEAN"
-build_clean
+rm -rf $KERNDIR/out_bootimg $KERNDIR_OUT
+mkdir $KERNDIR/out_bootimg $KERNDIR_OUT $INITRAM_DIR
 echo_notify "----------------------------------------------------------------------------------------------------------CONFIG"
-build_defconfig $DEFCONFIG
+make $DEFCONFIG menuconfig O=$KERNDIR_OUT
 echo_notify "----------------------------------------------------------------------------------------------------------BUILD"
 make -j$jobn O=$KERNDIR_OUT
 if [ ! -e $KERNDIR_OUT/arch/arm/boot/zImage ]
@@ -106,10 +70,27 @@ then
 	exit
 fi
 echo_notify "----------------------------------------------------------------------------------------------------------INITRAMFS"
-build_initramfs $INITRAM_ORIG
+# copy original initramfs directory to kernel directory
+cp -r $INITRAM_ORIG/* $INITRAM_DIR/
+# remove backup files
+find $INITRAM_DIR -name "*~" -exec rm -rf {} \;
+
+# find and copy modulized files
+for module_file in `find $KERNDIR_OUT -name "*.ko"`
+do
+	echo $module_file
+	"$CROSS_COMPILE"strip --strip-unneeded $module_file
+	cp $module_file $INITRAM_DIR/lib/modules/
+done
+# make initramfs file
+cd $INITRAM_DIR
+find . | cpio -o -H newc | gzip > $KERNDIR/out_bootimg/ramdisk.cpio.gz
+
 echo_notify "----------------------------------------------------------------------------------------------------------BOOTIMG"
 cd $KERNDIR/out_bootimg
-build_bootimg
+
+$KERNDIR/mkbootimg --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --kernel $KERNDIR_OUT/arch/arm/boot/zImage --ramdisk ramdisk.cpio.gz -o boot.img
+
 if [ -e $KERNDIR/out_bootimg/boot.img ]
 then
 	echo_info "Build Complete"
