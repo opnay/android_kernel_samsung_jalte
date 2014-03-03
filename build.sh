44ba52cda325b3a3a4498897436886d4d0f9f3a0
OP_Nay <opnayn@gmail.com>
2014-03-03 14:20:14 +0900

    build: Update annotations

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 export ARCH=arm
-export CROSS_COMPILE=/home/op_nay/toolchain/android-toolchain-eabi-4.8-14.01/bin/arm-eabi-
-#get kernel directory(current directory) from terminal
+export CROSS_COMPILE=/home/op_nay/toolchain/android-toolchain-eabi-4.8-14.01/bin/arm-eabi- ##### Set your toolchain directory
 KERNDIR=`pwd`
 KERNDIR_OUT=$KERNDIR/out
-INITRAM_ORIG=/home/op_nay/android/initram
-INITRAM_DIR=$KERNDIR_OUT/initramfs
+INITRAM_ORIG=/home/op_nay/android/initram ##### original ramdisk directory
+INITRAM_DIR=$KERNDIR_OUT/initramfs ##### Tmp directory for 
 DEVICE_CARRIER=$1
-jobn=16
+jobn=16 ##### Your CPU core * 2
 
-#Set Color
+##### Set output color function
+
 function echo_error() {
 	echo -e "\e[01m\e[31m$* \e[00m"
 }
@@ -25,6 +25,8 @@ function echo_notify() {
 	echo -e "\e[33m$*\e[00m"
 }
 
+##### Check args...
+
 if [ "$DEVICE_CARRIER" == "" ]
 then
 	echo_error "build.sh [ skt / kt / lg ]"
@@ -35,6 +37,8 @@ else
 fi
 DEFCONFIG=immortal_"$DEVICE_CARRIER"_defconfig
 
+##### Showing script config values
+
 echo_notify "Check Settings"
 echo_info "Carrier : " $DEVICE_CARRIER
 echo_info "ARCH : " $ARCH
@@ -43,6 +47,7 @@ echo_info "Tool Chain : " $CROSS_COMPILE
 echo_info "Initramfs Source : " $INITRAM_ORIG
 echo_info "Defconfig : " $DEFCONFIG
 
+##### Checking ramdisk and defconfigs
 
 if [ -e $INITRAM_ORIG ]
 then
@@ -58,38 +63,42 @@ then
         exit 1
 fi
 
+##### Remove files which before built
 
-echo_notify "----------------------------------------------------------------------------------------------------------CLEAN"
 rm -rf $KERNDIR/out_bootimg $KERNDIR_OUT
 mkdir $KERNDIR/out_bootimg $KERNDIR_OUT $INITRAM_DIR
-echo_notify "----------------------------------------------------------------------------------------------------------CONFIG"
 make $DEFCONFIG menuconfig O=$KERNDIR_OUT
-echo_notify "----------------------------------------------------------------------------------------------------------BUILD"
 make -j$jobn O=$KERNDIR_OUT
 if [ ! -e $KERNDIR_OUT/arch/arm/boot/zImage ]
 then
 	echo_error "Error occured"
 	exit
 fi
-echo_notify "----------------------------------------------------------------------------------------------------------INITRAMFS"
-# copy original initramfs directory to kernel directory
+
+##### Copy ramdisk to tmp folder on kernel directory
+
 cp -r $INITRAM_ORIG/* $INITRAM_DIR/
 find $INITRAM_DIR -name EMPTY -exec rm -rf {} \;
 find $INITRAM_DIR -name "*~" -exec rm -rf {} \;
 
-# find and copy modulized files
+##### find and copy *.ko files(module files)
+
 for module_file in `find $KERNDIR_OUT -name "*.ko"`
 do
 	echo $module_file
 	"$CROSS_COMPILE"strip --strip-unneeded $module_file
 	cp $module_file $INITRAM_DIR/lib/modules/
 done
-# make initramfs file
+
+##### Compressing ramdisk Directory
+
 cd $INITRAM_DIR
 find . | cpio -o -H newc | gzip > $KERNDIR/out_bootimg/ramdisk.cpio.gz
 
-echo_notify "----------------------------------------------------------------------------------------------------------BOOTIMG"
 cd $KERNDIR/out_bootimg
+
+
+##### Make Bootimg
 
 $KERNDIR/mkbootimg --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --kernel $KERNDIR_OUT/arch/arm/boot/zImage --ramdisk ramdisk.cpio.gz -o boot.img
 
