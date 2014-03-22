f537fed9850bdc3cceb543988f0ac8ca7826ada4
OPNay <opnayn@gmail.com>
2014-03-22 22:44:58 +0900

    build: Update build.sh and include binary

--- build.sh
+++ build.sh
@@ -1,118 +1,80 @@
+source build_function.sh
+source build_export.sh
+if [ "$1" == "" ]
+then
+	Error "USE : build.sh <skt/kt/lg>"
+	exit
+fi
+
 export ARCH=arm
-export CROSS_COMPILE=/home/op_nay/toolchain/android-toolchain-eabi-4.8-14.01/bin/arm-eabi- ##### Set your toolchain directory
-KERNDIR=`pwd`
-KERNDIR_OUT=$KERNDIR/out
-INITRAM_ORIG=/home/op_nay/android/initram ##### original ramdisk directory
-INITRAM_DIR=$KERNDIR_OUT/initramfs ##### Tmp directory for 
-DEVICE_CARRIER=$1
-jobn=16 ##### Your CPU core * 2
+export CROSS_COMPILE=$TOOLCHAIN
 
-##### Set output color function
+DEVICE_CA=$1
+DEFCONFIG=immortal_"$DEVICE_CA"_defconfig
+RAMDISK_ORIG=/project/ramdisk/$1
 
-function echo_error() {
-	echo -e "\e[01m\e[31m$* \e[00m"
-}
 
-function echo_info() {
-	echo -e "\e[36m$1 \e[00m"
-	if [ "$2" != "" ]
-	then
-		echo -e "\t\e[32m$2 \e[00m"
-	fi
-}
 
-function echo_notify() {
-	echo -e "\e[33m$*\e[00m"
-}
-
-##### Check args...
-
-if [ "$DEVICE_CARRIER" == "" ]
+echo
+ShowInfo "Check Script Settings"
+echo
+ShowInfo "Kernel Directory :" $KERNEL_DIR
+ShowInfo "Kernel Output Directory :" $KERNEL_OUT_DIR
+ShowInfo "Kernel Output boot.img Directory :" $KERNEL_BOOTIMG_DIR
+ShowInfo "Ramdisk Directory :" $RAMDISK_ORIG
+if [ ! -e $RAMDISK_ORIG ]
 then
-	echo_error "build.sh [ skt / kt / lg ]"
-	echo_error "Initramfs directory will be set \"$INITRAM_ORIG/<skt / kt / lg>\""
+	Error "Ramdisk Directory Not Found!"
 	exit
-else
-	INITRAM_ORIG="$INITRAM_ORIG/$DEVICE_CARRIER"
 fi
-DEFCONFIG=immortal_"$DEVICE_CARRIER"_defconfig
-
-##### Showing script config values
-
-echo_notify "Check Settings"
-echo_info "Carrier : " $DEVICE_CARRIER
-echo_info "ARCH : " $ARCH
-echo_info "Kernel Directory : " $KERNDIR
-echo_info "Tool Chain : " $CROSS_COMPILE
-echo_info "Initramfs Source : " $INITRAM_ORIG
-echo_info "Defconfig : " $DEFCONFIG
-
-##### Checking ramdisk and defconfigs
-
-if [ -e $INITRAM_ORIG ]
+ShowInfo "Toolchain : " $CROSS_COMPILE
+if [ ! -e "$CROSS_COMPILE"ld ]
 then
-        echo_info "Ramdisk is exist"
-else
-        echo_error "No such directory"
-        exit 1
+	Error "Toolchain Not found!"
+	exit
 fi
-
-if [ ! -e "$KERNDIR/arch/arm/configs/$DEFCONFIG" ]
+ShowInfo "Number of CPU Core : " $NB_CPU
+ShowInfo "Defconfig : " $DEFCONFIG
+if [ ! -e $KERNEL_DIR/arch/arm/configs/$DEFCONFIG ]
 then
-        echo_error "Configuration file $DEFCONFIG don't exists"
-        exit 1
-fi
-
-##### Remove files which before built
-
-rm -rf $KERNDIR/out_bootimg $KERNDIR_OUT
-mkdir $KERNDIR/out_bootimg $KERNDIR_OUT $INITRAM_DIR
-make $DEFCONFIG menuconfig O=$KERNDIR_OUT
-make -j$jobn O=$KERNDIR_OUT
-if [ ! -e $KERNDIR_OUT/arch/arm/boot/zImage ]
-then
-	echo_error "Error occured"
+	Error "Defconfig Not Found!"
 	exit
 fi
 
-##### Copy ramdisk to tmp folder on kernel directory
 
-cp -r $INITRAM_ORIG/* $INITRAM_DIR/
-find $INITRAM_DIR -name EMPTY -exec rm -rf {} \;
-find $INITRAM_DIR -name "*~" -exec rm -rf {} \;
+## Clean output Directory
+rm -rf $KERNEL_BOOTIMG_DIR $KERNEL_OUT_DIR
+mkdir -p $KERNEL_BOOTIMG_DIR $KERNEL_OUT_DIR $RAMDISK_OUT_DIR
 
-##### find and copy *.ko files(module files)
+## Make
+ShowNoty "==Make Config"
+make $DEFCONFIG -j$NB_CPU O=$KERNEL_OUT_DIR
+#make menuconfig -j$NB_CPU O=$KERNEL_OUT_DIR
+
+ShowNoty "==Start Make"
+make -j$NB_CPU O=$KERNEL_OUT_DIR
+
+if [ ! -e $KERNEL_OUT_DIR/arch/arm/boot/zImage ]
+then
+	Error "Error occured!"
+	exit
+fi
+
+## Copy Ramdisk
+ShowNoty "==Make Ramdisk"
+cp -r $RAMDISK_ORIG/* $RAMDISK_OUT_DIR/
+find $RAMDISK_OUT_DIR -name EMPTY -exec rm -rf {} \;
+find $RAMDISK_OUT_DIR -name "*~" -exec rm -rf {} \;
 
 for module_file in `find $KERNDIR_OUT -name "*.ko"`
 do
 	echo $module_file
-	"$CROSS_COMPILE"strip --strip-unneeded $module_file
-	cp $module_file $INITRAM_DIR/lib/modules/
+	$STRIP --strip-unneeded $module_file
+	cp $module_file $RAMDISK_OUT_DIR/lib/modules/
 done
 
-##### Compressing ramdisk Directory
-
-cd $INITRAM_DIR
-find . | cpio -o -H newc | gzip > $KERNDIR/out_bootimg/ramdisk.cpio.gz
-
-cd $KERNDIR/out_bootimg
-
-
-##### Make Bootimg
-
-$KERNDIR/mkbootimg --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --kernel $KERNDIR_OUT/arch/arm/boot/zImage --ramdisk ramdisk.cpio.gz -o boot.img
-
-if [ -e $KERNDIR/out_bootimg/boot.img ]
-then
-	echo_info "Build Complete"
-else
-	echo_error "Couldn't make boot.img"
-	exit
-fi
-
-if [ "$DEVICE_CARRIER" = "lg" ]
-then
-	DEVICE_CARRIER=U
-fi
-
-$KERNDIR/build_md5.sh ImmortalKernel-140225-`echo $DEVICE_CARRIER | tr -s [:lower:] [:upper:]`-Odin  boot.img
+ShowNoty "==Make Boot.img"
+$MKBOOTFS $RAMDISK_OUT_DIR > $KERNEL_BOOTIMG_DIR/ramdisk-boot.cpio
+$MINIGZIP < $KERNEL_BOOTIMG_DIR/ramdisk-boot.cpio > $KERNEL_BOOTIMG_DIR/ramdisk-boot.cpio.gz
+$MKBOOTIMG --base 0x10000000 --pagesize 2048 --kernel $KERNEL_OUT_DIR/arch/arm/boot/zImage --ramdisk $KERNEL_BOOTIMG_DIR/ramdisk-boot.cpio.gz -o $KERNEL_BOOTIMG_DIR/boot.img
+ShowNoty "==Complete Goto $KERNEL_BOOTIMG_DIR"
---
+++ build_function.sh
@@ -0,0 +1,11 @@
+function Error() {
+	echo -e "\e[01m\e[31m$* \e[00m"
+}
+
+function ShowInfo() {
+	echo -e "\e[32m\e[01m$1 \e[30m$2 \e[00m"
+}
+
+function ShowNoty() {
+	echo -e "\e[36m\e[01m$*\e[00m"
+}
