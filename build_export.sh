bf313f74bcac5201af19512a378c397c3f6ec7f8
OPNay <opnayn@gmail.com>
2014-05-29 00:11:06 +0900

    build_export: Update gcc4.9

--- build_export.sh
+++ build_export.sh
@@ -7,7 +7,7 @@ RAMDISK_ORIG=/project/android/kernel/ramdisk
 RAMDISK_OUT_DIR=$KERNEL_OUT_DIR/ramdisk
 
 ## Binary
-TOOLCHAIN=/project/toolchain/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin/arm-linux-gnueabihf-
+TOOLCHAIN=/project/toolchain/android-toolchain-eabi-4.9-14.05/bin/arm-eabi-
 MINIGZIP=$KERNEL_BIN_DIR/minigzip
 MKBOOTFS=$KERNEL_BIN_DIR/mkbootfs
 MKBOOTIMG=$KERNEL_BIN_DIR/mkbootimg
