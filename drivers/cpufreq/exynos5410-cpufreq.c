d1e6e210a82b1b10b66405008b8cd2a98b7970ba
OPNay <opnayn@gmail.com>
2014-06-11 21:35:46 +0900

    cpufreq: exynos5410-cpufreq: Allow CA7 frequency L8(500Mhz) -> L11(200Mhz) and order some lines

--- drivers/cpufreq/exynos5410-cpufreq.c
+++ drivers/cpufreq/exynos5410-cpufreq.c
@@ -811,8 +811,6 @@ static void __init set_volt_table_CA7(void)
 				exynos5410_volt_table_CA7[i]);
 	}
 
-	max_support_idx_CA7 = L1;
-
 	exynos5410_freq_table_CA7[L0].frequency = CPUFREQ_ENTRY_INVALID;
 	if (samsung_rev() < EXYNOS5410_REV_2_0) {
 		exynos5410_freq_table_CA7[L1].frequency = CPUFREQ_ENTRY_INVALID;
@@ -823,13 +821,11 @@ static void __init set_volt_table_CA7(void)
 		exynos5410_freq_table_CA7[L6].frequency = CPUFREQ_ENTRY_INVALID;
 
 		max_support_idx_CA7 = L7;
+	} else {
+		max_support_idx_CA7 = L1;
 	}
 
-	exynos5410_freq_table_CA7[L9].frequency = CPUFREQ_ENTRY_INVALID;
-	exynos5410_freq_table_CA7[L10].frequency = CPUFREQ_ENTRY_INVALID;
-	exynos5410_freq_table_CA7[L11].frequency = CPUFREQ_ENTRY_INVALID;
-
-	min_support_idx_CA7 = L8;
+	min_support_idx_CA7 = L11;
 }
 
 static void __init set_volt_table_CA15(void)
@@ -853,12 +849,13 @@ static void __init set_volt_table_CA15(void)
 				exynos5410_volt_table_CA15[i]);
 	}
 
-	max_support_idx_CA15 = L4;
-
 	exynos5410_freq_table_CA15[L0].frequency = CPUFREQ_ENTRY_INVALID;
 	exynos5410_freq_table_CA15[L1].frequency = CPUFREQ_ENTRY_INVALID;
 	exynos5410_freq_table_CA15[L2].frequency = CPUFREQ_ENTRY_INVALID;
 	exynos5410_freq_table_CA15[L3].frequency = CPUFREQ_ENTRY_INVALID;
+
+	exynos5410_freq_table_CA15[L17].frequency = CPUFREQ_ENTRY_INVALID;
+	exynos5410_freq_table_CA15[L18].frequency = CPUFREQ_ENTRY_INVALID;
 	if (samsung_rev() < EXYNOS5410_REV_2_0) {
 		exynos5410_freq_table_CA15[L4].frequency = CPUFREQ_ENTRY_INVALID;
 		exynos5410_freq_table_CA15[L5].frequency = CPUFREQ_ENTRY_INVALID;
@@ -868,23 +865,17 @@ static void __init set_volt_table_CA15(void)
 		exynos5410_freq_table_CA15[L9].frequency = CPUFREQ_ENTRY_INVALID;
 
 		max_support_idx_CA15 = L10;
-	}
-
-	if (samsung_rev() < EXYNOS5410_REV_2_0) {
-		exynos5410_freq_table_CA15[L17].frequency = CPUFREQ_ENTRY_INVALID;
-		exynos5410_freq_table_CA15[L18].frequency = CPUFREQ_ENTRY_INVALID;
-
 		min_support_idx_CA15 = L16;
 	} else {
 		exynos5410_freq_table_CA15[L13].frequency = CPUFREQ_ENTRY_INVALID;
 		exynos5410_freq_table_CA15[L14].frequency = CPUFREQ_ENTRY_INVALID;
 		exynos5410_freq_table_CA15[L15].frequency = CPUFREQ_ENTRY_INVALID;
 		exynos5410_freq_table_CA15[L16].frequency = CPUFREQ_ENTRY_INVALID;
-		exynos5410_freq_table_CA15[L17].frequency = CPUFREQ_ENTRY_INVALID;
-		exynos5410_freq_table_CA15[L18].frequency = CPUFREQ_ENTRY_INVALID;
 
+		max_support_idx_CA15 = L4;
 		min_support_idx_CA15 = L12;
 	}
+
 }
 
 int __init exynos5410_cpufreq_CA7_init(struct exynos_dvfs_info *info)
