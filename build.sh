ede9caa4b605d65703a134156a1d29482e1d037d
OPNay <opnayn@gmail.com>
2014-07-03 00:03:12 +0900

    Add build script

---
+++ build.sh
@@ -0,0 +1,80 @@
+source build_function.sh
+source build_export.sh
+if [ "$1" == "" ]
+then
+	Error "USE : build.sh <skt/kt/lg>"
+	exit
+fi
+
+export ARCH=arm
+export CROSS_COMPILE=$TOOLCHAIN
+
+DEVICE_CA=$1
+DEFCONFIG=immortal_"$DEVICE_CA"_defconfig
+RAMDISK_ORIG=$RAMDISK_ORIG/$1
+
+
+
+echo
+ShowInfo "Check Script Settings"
+echo
+ShowInfo "Kernel Directory :" $KERNEL_DIR
+ShowInfo "Kernel Output Directory :" $KERNEL_OUT_DIR
+ShowInfo "Kernel Output boot.img Directory :" $KERNEL_BOOTIMG_DIR
+ShowInfo "Ramdisk Directory :" $RAMDISK_ORIG
+if [ ! -e $RAMDISK_ORIG ]
+then
+	Error "Ramdisk Directory Not Found!"
+	exit
+fi
+ShowInfo "Toolchain : " $CROSS_COMPILE
+if [ ! -e "$CROSS_COMPILE"ld ]
+then
+	Error "Toolchain Not found!"
+	exit
+fi
+ShowInfo "Number of CPU Core : " $NB_CPU
+ShowInfo "Defconfig : " $DEFCONFIG
+if [ ! -e $KERNEL_DIR/arch/arm/configs/$DEFCONFIG ]
+then
+	Error "Defconfig Not Found!"
+	exit
+fi
+
+
+## Clean output Directory
+rm -rf $KERNEL_BOOTIMG_DIR $KERNEL_OUT_DIR
+mkdir -p $KERNEL_BOOTIMG_DIR $KERNEL_OUT_DIR $RAMDISK_OUT_DIR
+
+## Make
+ShowNoty "==Make Config"
+make $DEFCONFIG -j$NB_CPU O=$KERNEL_OUT_DIR
+#make menuconfig -j$NB_CPU O=$KERNEL_OUT_DIR
+
+ShowNoty "==Start Make"
+make -j$NB_CPU O=$KERNEL_OUT_DIR
+
+if [ ! -e $KERNEL_OUT_DIR/arch/arm/boot/zImage ]
+then
+	Error "Error occured!"
+	exit
+fi
+
+## Copy Ramdisk
+ShowNoty "==Make Ramdisk"
+cp -r $RAMDISK_ORIG/* $RAMDISK_OUT_DIR/
+find $RAMDISK_OUT_DIR -name EMPTY -exec rm -rf {} \;
+find $RAMDISK_OUT_DIR -name "*~" -exec rm -rf {} \;
+
+for module_file in `find $KERNDIR_OUT -name "*.ko"`
+do
+	echo $module_file
+	$STRIP --strip-unneeded $module_file
+	cp $module_file $RAMDISK_OUT_DIR/lib/modules/
+done
+
+ShowNoty "==Make Boot.img"
+$MKBOOTFS $RAMDISK_OUT_DIR > $KERNEL_BOOTIMG_DIR/ramdisk-boot.cpio
+$MINIGZIP < $KERNEL_BOOTIMG_DIR/ramdisk-boot.cpio > $KERNEL_BOOTIMG_DIR/ramdisk-boot.cpio.gz
+$MKBOOTIMG --base 0x10000000 --pagesize 2048 --kernel $KERNEL_OUT_DIR/arch/arm/boot/zImage --ramdisk $KERNEL_BOOTIMG_DIR/ramdisk-boot.cpio.gz -o $KERNEL_BOOTIMG_DIR/boot.img
+ShowNoty "==Complete Goto $KERNEL_BOOTIMG_DIR"
---
+++ build_export.sh
@@ -0,0 +1,16 @@
+## Kernel Directory
+KERNEL_DIR=`pwd`
+KERNEL_OUT_DIR=`pwd`/out
+KERNEL_BOOTIMG_DIR=`pwd`/bootimg
+KERNEL_BIN_DIR=`pwd`/bin
+RAMDISK_ORIG=/project/android/kernel/ramdisk
+RAMDISK_OUT_DIR=$KERNEL_OUT_DIR/ramdisk
+
+## Binary
+TOOLCHAIN=/project/toolchain/gcc-linaro-arm-linux-gnueabihf-4.9-2014.05_linux/bin/arm-linux-gnueabihf-
+MINIGZIP=$KERNEL_BIN_DIR/minigzip
+MKBOOTFS=$KERNEL_BIN_DIR/mkbootfs
+MKBOOTIMG=$KERNEL_BIN_DIR/mkbootimg
+STRIP="$TOOLCHAIN"strip
+
+NB_CPU=16 #`grep process /proc/cpuinfo | wc -l`
