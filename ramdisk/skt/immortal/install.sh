22f510ec1153e43eb05c284a64b017c717502485
OPNay <opnayn@gmail.com>
2014-07-28 15:28:09 +0900

    Ramdisk: SKT: Fix install Immortal Application

--- ramdisk/skt/immortal/install.sh
+++ ramdisk/skt/immortal/install.sh
@@ -1,12 +1,23 @@
 #!/immortal/bin/busybox sh
 PATH=/immortal/bin:$PATH
 
-if [ -e "/system/app/Immortal Kernel.apk" ]; then exit; fi
+# Wait for mount system
+while [[ "$(mount | grep '/system')" == "" || "$(mount | grep '/data')" == "" ]]; do
+	echo "Not mount /system and /data"
+	sleep 1
+done
+
+if [ ! -e "/data/media/0/immortal/install" ]; then exit; fi
+
+if [ -e "/system/app/ImmortalKernel.apk" ]; then exit; fi
+
 
 mount -o rw,remount /system
 
-cp "/immortal/Immortal Kernel.apk" "/system/app/Immortal Kernel.apk"
-chmod 644 /system/app/Immortal Kernel.apk
-chown root.root /system/app/Immortal Kernel.apk
+cp -f "/immortal/ImmortalKernel.apk" "/system/app/"
+chmod 644 /system/app/ImmortalKernel.apk
+chown root.root /system/app/ImmortalKernel.apk
 
 mount -o ro,remount /system
+
+exit
