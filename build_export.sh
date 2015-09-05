a53069cf9d95a690cb59836eabc841e4b07f5a93
Inseop Kim <opnayn@gmail.com>
2015-09-05 18:01:44 +0900

    mkbootimg: Update mkbootimg. bring from android platform_system_core repository.

--- build_export.sh
+++ build_export.sh
@@ -16,7 +16,7 @@ RAMDISK_DIR_ORIG=$KERNEL_DIR/ramdisk
 TOOLCHAIN_PREFIX=/workspace/bin/android-toolchain-eabi-5.2.0-x86/bin/arm-eabi-
 strip="$TOOLCHAIN_PREFIX"strip
 mkbootfs="$KERNEL_BIN/mkbootfs"
-mkbootimg="$KERNEL_BIN/mkbootimg"
+mkbootimg="$KERNEL_DIR/scripts/mkbootimg"
 mkbootimg_args="--base 0x10000000 \
     --ramdisk_offset 0x01000000 \
     --pagesize 2048"
