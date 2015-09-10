80a3b890e501f89d1add3907ebcf3ba5231a23ef
Inseop Kim <opnayn@gmail.com>
2015-09-10 17:39:28 +0900

    build: Update ramdisk_fix_permissions.sh fix some permissions.

--- ramdisk_fix_permissions.sh
+++ ramdisk_fix_permissions.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-chmod 755 init*
+chmod 750 init*
 chmod 644 *.rc
 
 chmod 644 *contexts
 chmod 644 sepolicy*
 chmod 644 fstab*
+
 chmod 644 default.prop
 
-chmod 755 dev
-chmod 755 proc
-chmod 777 res
-chmod 755 res/*
-chmod 750 sbin
-chmod 755 sbin/*
-chmod 755 sys
-
-chmod 755 *.sh
+chmod -R 750 sbin res
+chmod -R 755 dev proc sys system
