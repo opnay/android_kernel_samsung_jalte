f3b58e25ce59e654ce7a521a7958b3b91f39638e
Alberto Guanti <albertoguanti@gmail.com>
2017-02-05 23:49:30 +0100

    cm_i9500: script: switch to cyanogenmod defconfig

--- build_kernel_test.sh
+++ build_kernel_test.sh
@@ -3,5 +3,5 @@
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.8/bin/arm-eabi-
 
-make -j8 ARCH=arm ja3g_00_defconfig
+make -j8 ARCH=arm cyanogenmod_i9500_defconfig
 make -j8 ARCH=arm
