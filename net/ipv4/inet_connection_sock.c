a40619fe20e9504383f86b3cfee0d03484f0f8f9
Eric Dumazet <edumazet@google.com>
2017-05-09 06:29:19 -0700

    CVE-2017-8890: dccp/tcp: do not inherit mc_list from parent
    syzkaller found a way to trigger double frees from ip_mc_drop_socket()
    
    It turns out that leave a copy of parent mc_list at accept() time,
    which is very bad.
    
    Very similar to commit 8b485ce69876 ("tcp: do not inherit
    fastopen_req from parent")
    
    Initial report from Pray3r, completed by Andrey one.
    Thanks a lot to them !
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: Pray3r <pray3r.z@gmail.com>
    Reported-by: Andrey Konovalov <andreyknvl@google.com>
    Tested-by: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Change-Id: I2eac7b825a5b597af14a0573b76b685131c46726

--- net/ipv4/inet_connection_sock.c
+++ net/ipv4/inet_connection_sock.c
@@ -620,6 +620,8 @@ struct sock *inet_csk_clone_lock(const struct sock *sk,
 		
 		newsk->sk_mark = inet_rsk(req)->ir_mark;
 
+		inet_sk(newsk)->mc_list = NULL;
+
 		newsk->sk_mark = inet_rsk(req)->ir_mark;
 
 		newicsk->icsk_retransmits = 0;
