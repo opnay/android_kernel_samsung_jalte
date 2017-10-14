79ed5004d54413cadf1601324e0ecac5dfe363b8
Alberto Guanti <albertoguanti@gmail.com>
2017-10-14 12:54:16 +0200

    exynos5410: arch: arm: ja3gxx: switch to interactive cpu governor

--- build_kernel_test.sh
+++ build_kernel_test.sh
@@ -3,5 +3,5 @@
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.8/bin/arm-eabi-
 
-make -j8 ARCH=arm lineageos_i9500_defconfig
+make -j8 ARCH=arm lineageos_ja3gxx_defconfig
 make -j8 ARCH=arm
