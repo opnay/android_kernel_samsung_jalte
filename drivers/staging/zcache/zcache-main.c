44f0888c891acc423e31ed3d22d514ca08e25a6d
Kim In Seop <opnayn@gmail.com>
2017-11-06 20:49:02 +0900

    drivers: staging: Remove zsmalloc this moved to mm/zsmalloc.c commit(c3ebd97a9f88d95d4b7fdaeeeefa0e28fcafae69): "Drop OSRC E300S LL Sources"

--- drivers/staging/zcache/zcache-main.c
+++ drivers/staging/zcache/zcache-main.c
@@ -31,9 +31,8 @@
 #include <linux/math64.h>
 #include <linux/crypto.h>
 #include <linux/string.h>
+#include <linux/zsmalloc.h>
 #include "tmem.h"
-
-#include "../zsmalloc/zsmalloc.h"
 
 #if (!defined(CONFIG_CLEANCACHE) && !defined(CONFIG_FRONTSWAP))
 #error "zcache is useless without CONFIG_CLEANCACHE or CONFIG_FRONTSWAP"
