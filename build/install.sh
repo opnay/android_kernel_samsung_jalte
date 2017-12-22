796a796b8dc9f0327bf2cfd2a4a7550669e7175b
Kim In Seop <opnayn@gmail.com>
2017-12-22 19:33:37 +0900

    build: install: Update can flash with heimdall

--- build/install.sh
+++ build/install.sh
@@ -2,17 +2,14 @@
 ## If you have sdk, set adb command line
 #ADB=/your/adb/path
 adb="$(which adb)"
-adb_start="$adb start-server"
-adb_wait="$adb wait-for-device"
-adb_push="$adb push"
 adb_shell="$adb shell"
-adb_reboot="$adb reboot"
 
 # boot.img binary
 DEVPATH=/dev/block/mmcblk0p9
 
 function help() {
-	echo -e "install.sh [-s|--su] [-h|--help] <boot.img>\n"
+	echo -e "install.sh [--heimdall] [-s|--su] [-h|--help] <boot.img>\n"
+	echo -e "  --heimdall\tFlash with heimdall"
 	echo -e "  -s, --su\tRun shell with su"
 	echo -e "  -r\tInstall to Recovery partition(Default: /dev/block/platform/dw_mmc.0/by-name/RECOVERY)"
 	echo -e "  -h, --help\tShow this help message"
@@ -25,6 +22,7 @@ fi
 
 for tmp in $@; do
 	case $tmp in
+	    --heimdall) use_heimdall=true;;
 		-s | --su) adb_shell="$adb_shell su -c";;
 		-r) DEVPATH=/dev/block/platform/dw_mmc.0/by-name/RECOVERY && adb_reboot="$adb_reboot recovery";;
 		-h | --help) help;;
@@ -39,28 +37,40 @@ if [ ! -e $adb ] || [ "$adb" == "" ]; then
 fi
 
 echo -e "BOOTIMG : $BOOTIMG"
-if [ -d $BOOTIMG ]; then
-	echo "$BOOTIMG is directory."
-	exit
-fi
-if [ ! -e $BOOTIMG ] || [ "$BOOTIMG" = "" ]; then
-	echo -e "boot.img was not found"
+if [ -d $BOOTIMG ] || [ ! -e $BOOTIMG ] || [ "$BOOTIMG" = "" ]; then
+	echo -e "boot.img was not found!!"
 	exit
 fi
 
-echo -e "* Start adb server"
-$adb_start
+if [ "$use_heimdall" == "true" ]; then
+	echo ""
+	echo "# Wait for reboot device."
+	echo "# If it does not work,"
+	echo "#   Check enable 'USB Debugging' on your device."
+	echo ""
+	$adb wait-for-device && $adb reboot download
+	echo ""
+	echo "# Adb disconnected!"
+	echo "# Now, wait 5sec for download mode"
+	echo ""
+	sleep 5
+    heimdall flash --BOOT $BOOTIMG
+	echo "# done!"
+else
+	echo -e "* Start adb server"
+	$adb start-server
 
-echo -e "* Wait for device"
-$adb_wait
+	echo -e "* Wait for device"
+	$adb wait-for-device
 
-echo -e "* Push boot.img to device (in /data/local/tmp)"
-$adb_push $BOOTIMG /data/local/tmp/boot.img
+	echo -e "* Push boot.img to device (in /data/local/tmp)"
+	$adb push $BOOTIMG /data/local/tmp/boot.img
 
-echo -e "* Install..."
-$adb_shell dd if=/data/local/tmp/boot.img of=$DEVPATH
+	echo -e "* Install..."
+	$adb_shell dd if=/data/local/tmp/boot.img of=$DEVPATH
 
-echo -e "* Reboot!"
-$adb_reboot
+	echo -e "* Reboot!"
+	$adb reboot
 
-echo -e "* Finish Install"
+	echo -e "* Finish Install"
+fi
